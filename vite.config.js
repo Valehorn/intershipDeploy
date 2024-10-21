d37b5b1c44d4ce86bafedd2ec43d65b717014a0d
Пакри <pakri@yandex.ru>
2024-10-21 15:37:33 +0300

    Деплой vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,6 @@ export default {
       styles: false,
       injectSVGOnDev: true,
     }),
-    // input https://www.npmjs.com/package/html-minifier-terser options
-    // ViteMinifyPlugin({}),
     ViteImageOptimizer({
       test: /\.(jpe?g|png|svg)$/i,
       includePublic: false,
@@ -30,7 +28,7 @@ export default {
                   forceAbsolutePath: false,
                   utilizeAbsolute: false,
                 },
-                removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+                removeViewBox: false,
                 cleanupIds: false,
               },
             },
@@ -39,21 +37,17 @@ export default {
         ],
       },
       png: {
-        // https://sharp.pixelplumbing.com/api-output#png
         quality: 80,
         palette: true
       },
       jpeg: {
-        // https://sharp.pixelplumbing.com/api-output#jpeg
         quality: 80,
         progressive: true
       },
       jpg: {
-        // https://sharp.pixelplumbing.com/api-output#jpeg
         quality: 80,
         progressive: true
       },
-      // Cache assets in cacheLocation. When enabled, reads and writes asset files with their hash suffix from the specified path.
       cache: true,
       cacheLocation: './.cache',
     }),
